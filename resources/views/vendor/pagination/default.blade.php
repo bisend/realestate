87abebfcb14a5a454645b2234b0ef23b40450a54
Yanchuk Serhiy <yanchukserhiy96@gmail.com>
2018-05-21 10:20:52 +0300

    pagination style

--- resources/views/vendor/pagination/default.blade.php
+++ resources/views/vendor/pagination/default.blade.php
@@ -2,9 +2,9 @@
     <ul class="pagination">
         {{-- Previous Page Link --}}
         @if ($paginator->onFirstPage())
-            <li class="pagination-icon"><i class="material-icons">chevron_left</i></li>
+            <!-- <li class="pagination-icon"><i class="fa fa-chevron-left" aria-hidden="true"></i></li> -->
         @else
-            <li class="waves-effect pagination-icon"><a href="{{ $paginator->previousPageUrl() }}" rel="prev"><i class="material-icons">chevron_left</i></a></li>
+            <li class="waves-effect pagination-icon"><a href="{{ $paginator->previousPageUrl() }}" rel="prev"><i class="fa fa-angle-left" aria-hidden="true"></i></a></li>
         @endif
 
         {{-- Pagination Elements --}}
@@ -28,9 +28,9 @@
 
         {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
-            <li class="waves-effect pagination-icon"><a href="{{ $paginator->nextPageUrl() }}" rel="next"><i class="material-icons">chevron_right</i></a></li>
+            <li class="waves-effect pagination-icon"><a href="{{ $paginator->nextPageUrl() }}" rel="next"><i class="fa fa-angle-right" aria-hidden="true"></i></a></li>
         @else
-            <li class="pagination-icon"><i class="material-icons">chevron_right</i></li>
+            <!-- <li class="pagination-icon"><i class="fa fa-chevron-right" aria-hidden="true"></i></li> -->
         @endif
     </ul>
 @endif
