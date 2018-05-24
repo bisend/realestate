87ba1c2f7afdab01a93ceaf633afbd1d5dc34e0c
Yanchuk Serhiy <yanchukserhiy96@gmail.com>
2018-05-24 10:09:45 +0300

    commit

--- resources/views/realstate/property.blade.php
+++ resources/views/realstate/property.blade.php
@@ -14,14 +14,14 @@
 						<h4>{{ $mainProperty->contentload['name'] }}</h4>
 						@if($mainProperty->sales == 1 && $mainProperty->rentals == 1)
 						<div class="right Property-ref">Property Reference: <span>{{ $mainProperty->property_info['property_reference'] }}</span></div>
-						<div class="property-price-single right">${{ $mainProperty->prices['service_charge'] }}<span> Per Month</span></div>
 						<div class="property-price-single right"> ${{ $mainProperty->prices['week'] }}<span> Per Week</span></div>
+						<div class="property-price-single right">${{ $mainProperty->prices['service_charge'] }}<span> Per Month</span></div>
 						<div class="property-price-single right"> ${{ $mainProperty->prices['service_charge'] }}<span> For Sale</span></div>
 					
 						@elseif($mainProperty->rentals == 1)
 						<div class="right Property-ref">Property Reference: <span>{{ $mainProperty->property_info['property_reference'] }}</span></div>
-						<div class="property-price-single right">${{ $mainProperty->prices['service_charge'] }}<span> Per Month</span></div>
 						<div class="property-price-single right"> ${{ $mainProperty->prices['week'] }}<span> Per Week</span></div>
+						<div class="property-price-single right">${{ $mainProperty->prices['service_charge'] }}<span> Per Month</span></div>
 						@elseif($mainProperty->sales == 1)
 						<div class="right Property-ref">Property Reference: <span>{{ $mainProperty->property_info['property_reference'] }}</span></div>
 						<div class="property-price-single right">${{ $mainProperty->prices['service_charge'] }}<span>Service Charge</span></div>
