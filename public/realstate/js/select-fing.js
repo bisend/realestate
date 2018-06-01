46aacf76dbadf41ff833bca403a6b5c1e3e28863
Den <godenis91@ukr.net>
2018-06-01 18:40:01 +0300

    fix search

--- public/realstate/js/select-fing.js
+++ public/realstate/js/select-fing.js
@@ -3,36 +3,50 @@ $('#search_sale-country').on('change', function () {
     console.log('SELECT');
     $('.select-any').attr('selected', false);
 
-    if( $(this).val() == 'Spain'){
+    var countrID = $(this).val();
+    console.log(countrID)
 
-        $( ".location-select option" ).each(function( index ) {
-            if($(this).hasClass('select-any')){
-                $(this).attr('selected', true);
-            }else{
-                $(this).attr('selected', false);
-            }
-        });
+    $( ".location-select option" ).each(function() {
+        if($(this).hasClass('country-'+countrID)){
+            console.log($(this).attr('class'))
+            $(this).show();
+            console.log(this)
+        }else{
+            $(this).hide();
+            console.log('SULAAAA')
+        }
+     });
 
-        console.log($(this).val());
-        // $('.select-any').attr('selected', true);
-        $('.country-gb').hide();
-        $('.country-sp').show();
-    }
+    // if( $(this).val() == 'Spain'){
+        
+    //     $( ".location-select option" ).each(function( index ) {
+    //         if($(this).hasClass('select-any')){
+    //             $(this).attr('selected', true);
+    //         }else{
+    //             $(this).attr('selected', false);
+    //         }
+    //     });
 
-    if( $(this).val() == 'Gibraltar'){
-        $( ".location-select option" ).each(function( index ) {
-            if($(this).hasClass('select-any')){
-                $(this).attr('selected', true);
-            }else{
-                $(this).attr('selected', false);
-            }
-        });
+    //     console.log($(this).val());
 
-        console.log($(this).val());
-        // $('.select-any').attr('selected', true);
-        $('.country-sp').hide();
-        $('.country-gb').show();
-    }
+        
+
+    // }
+
+    // if( $(this).val() == 'Gibraltar'){
+    //     $( ".location-select option" ).each(function( index ) {
+    //         if($(this).hasClass('select-any')){
+    //             $(this).attr('selected', true);
+    //         }else{
+    //             $(this).attr('selected', false);
+    //         }
+    //     });
+
+    //     console.log($(this).val());
+    //     // $('.select-any').attr('selected', true);
+    //     $('.country-sp').hide();
+    //     $('.country-gb').show();
+    // }
 })
 
 
