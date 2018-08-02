e31c585ff70e7c1d97936c65220e96d16c402ca9
bisend <vboychenko54@gmail.com>
2018-08-02 14:25:06 +0300

    changed meta description

--- resources/views/realstate/layout.blade.php
+++ resources/views/realstate/layout.blade.php
@@ -3,7 +3,7 @@
 <html lang="en">
   <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <meta name="description" content="Homely - Responsive Real Estate Template">
+  <meta name="description" content="Find a Property Gibraltar">
   <meta name="author" content="Rype Creative [Chris Gipple]">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
